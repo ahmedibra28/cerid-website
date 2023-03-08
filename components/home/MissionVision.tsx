f0bc322152909f9e524b3ba7cb8fd6ffe1accfe5
Ahmed <ahmaat19@gmail.com>
2023-03-08 13:40:34 +0300

    adding mission and vision

--- components/home/MissionVision.tsx
+++ components/home/MissionVision.tsx
@@ -11,21 +11,22 @@ const MissionVision = ({
   return (
     <div className='container p-5'>
       <div className='row g-5'>
-        <div className='col-lg-6 col-md-12 border border-top-0 border-end-0 border-bottom-0 border-primary border-5 shadow-sm py-2'>
-          <h1 className='text-primary'>Mission</h1>
-          <div
-            className='card-text text-secondary'
-            dangerouslySetInnerHTML={{
-              __html: mission?.content.rendered,
-            }}
-          />
-        </div>
-        <div className='col-lg-6 col-md-12 border border-top-0 border-end-0 border-bottom-0 border-primary border-5 shadow-sm py-2'>
+        <div className='col-lg-8 col-md-10 col-12 mx-auto text-center border border-top-0 border-end-0 border-bottom-0 border-primary border-5 shadow-sm py-2'>
           <h1 className='text-primary'>Vision </h1>
           <div
             className='card-text text-secondary'
             dangerouslySetInnerHTML={{
               __html: vision?.content.rendered,
+            }}
+          />
+        </div>
+
+        <div className='col-lg-8 col-md-10 col-12 mx-auto text-center border border-top-0 border-end-0 border-bottom-0 border-primary border-5 shadow-sm py-2'>
+          <h1 className='text-primary'>Mission</h1>
+          <div
+            className='card-text text-secondary'
+            dangerouslySetInnerHTML={{
+              __html: mission?.content.rendered,
             }}
           />
         </div>
