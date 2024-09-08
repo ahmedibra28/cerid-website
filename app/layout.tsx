0522faf28cc43d687e7c942adc8eb0dd7d787b7c
Ahmed <ahmaat19@gmail.com>
2024-09-08 16:11:57 +0300

    refactor: Update font family in globals.css and layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+// import { Inter } from 'next/font/google'
 import './globals.css'
 import Navigation from '@/components/navigation'
 
-const inter = Inter({ subsets: ['latin'] })
+// const inter = Inter({ subsets: ['latin'] })
 // change to arial font
 
 export const metadata: Metadata = {
@@ -18,7 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
-      <body className={inter.className}>
+      <body>
         <Navigation />
         <main>{children}</main>
       </body>
